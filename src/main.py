e73f758423c0947a440d50c2970620f1843d3f6c
남현석 <hyeonseok3168@gmail.com>
2025-09-24 16:54:59 +0900

    2차 제출_서버ip추가

--- src/main.py
+++ src/main.py
@@ -1,20 +1,30 @@
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
+from fastapi import Request
 
 from tests.util import get_all_src_py_files_hash
 from src.api import api_router
 from src.users.errors import MissingValueException
+from src.common.custom_exception import CustomException
 
 app = FastAPI()
 
 app.include_router(api_router)
 
 @app.exception_handler(RequestValidationError)
-def handle_request_validation_error(request, exc):
-    raise MissingValueException()
+def handle_request_validation_error(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=422,
+        content={"error_code": "ERR_001", "error_msg": "MISSING VALUE"},
+    )
 
-
+@app.exception_handler(CustomException)
+def handle_custom_exception(request: Request, exc: CustomException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"error_code": exc.error_code, "error_msg": exc.error_message},
+    )
 
 @app.get("/health")
 def health_check():
